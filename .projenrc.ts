6bf4040151be087a6ab28f2c00ab1dc899e4f2ca
mtahir-cloudkitect <mtahir@cloudkitect.com>
2024-05-31 21:50:24 -0700

    feat: Initial project setup

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,17 +1,41 @@
-import { cdk, javascript } from 'projen';
+import {cdk, javascript, ReleasableCommits, TextFile} from 'projen';
+import {NpmAccess} from "projen/lib/javascript";
+
+const nodeVersion = '20';
+
 const project = new cdk.JsiiProject({
   author: 'mtahir-cloudkitect',
-  authorAddress: 'mtahir@cloudkitect.com',
+  authorAddress: 'support@cloudkitect.com',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.4.0',
-  name: 'DeployableCdkApplication',
+  name: '@cloudkitect/deployable-cdk-application',
   packageManager: javascript.NodePackageManager.PNPM,
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/mtahir/DeployableCdkApplication.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  repositoryUrl: 'https://github.com/cloudkitect/deployable-cdk-application.git',
+  description: 'CloudKitect deployable cdk application',
+  keywords: [
+    'CloudKitect',
+    'PDK',
+    'Typescript',
+    'projen',
+    'Deployment',
+    'AWS CDK',
+    'Release CDK App',
+  ],
+  pnpmVersion: '9',
+  workflowNodeVersion: nodeVersion,
+  publishTasks: true,
+  npmAccess: NpmAccess.PUBLIC,
+  npmProvenance: false,
+  releasableCommits: ReleasableCommits.featuresAndFixes(),
+  deps: ['projen'],
+  peerDeps: ['projen', 'constructs'],
 });
+
+new TextFile(project, '.nvmrc', {
+  lines: [nodeVersion],
+});
+
+const exclusions = ['.DS_Store', '.idea', '*.iml'];
+project.gitignore.exclude(...exclusions);
 project.synth();
